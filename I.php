f6071a7ca95d0fe44ae2d21f8ea9c9ce0eafb2dc
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-11 21:31:54 +0300

    no message

--- I.php
+++ I.php
@@ -3,7 +3,7 @@ namespace Df\Facebook;
 use Df\Facebook\Settings as S;
 class I {
 	/** @return string */
-	public static function init() {return dfcf(function() {
+	static function init() {return dfcf(function() {
 		/** @var string|null $appId */
 		$appId = S::s()->appId();
 		return !$appId ? '' : df_phtml(__CLASS__, 'init', ['appId' => $appId]);
