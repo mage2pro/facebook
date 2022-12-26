2af8ad5f4e06ee2ba82d0e9b48e34e9092f6f438
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-26 12:42:23 +0000

    https://github.com/mage2pro/core/issues/168

--- I.php
+++ I.php
@@ -3,8 +3,8 @@ namespace Df\Facebook;
 use Df\Facebook\Settings as S;
 # 2016-04-09
 class I {
-	/** 2016-04-09 @return string */
-	static function init() {return dfcf(function() {
+	/** 2016-04-09 */
+	static function init():string {return dfcf(function():string {
 		$appId = S::s()->appId(); /** @var string|null $appId */
 		return !$appId ? '' : df_block_output(__CLASS__, 'init', ['appId' => $appId]);
 	});}
